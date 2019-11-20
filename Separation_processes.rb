f142ab11259180e1567cad8a1118ee90f5c08224
Irit Amel <iritamelch@gmail.com>
2019-11-20 16:54:59 +0200

    changing a variable name

--- Separation_processes.rb
+++ Separation_processes.rb
@@ -1,19 +1,19 @@
-def give_back_x_y(temp)
+def give_back_x_y(temps)
   ans = []
-  temp.each do |ele|
+  temp.each do |temp|
     # Antoine equations for wach component
-    antoine_for_acetone = 7.11714 - (1210.595 / (ele + 229.664))
-    antoine_for_ethanol = 8.11220 - (1592.864 / (ele + 226.184))
+    antoine_for_acetone = 7.11714 - (1210.595 / (temp + 229.664))
+    antoine_for_ethanol = 8.11220 - (1592.864 / (temp + 226.184))
     # Vapor pressure for each component
     p_acetone = 10**antoine_for_acetone
     p_ethanol = 10**antoine_for_ethanol
     x = (760 - p_ethanol) / (p_acetone - p_ethanol) # fraction of acetone in liquid
     y = (p_acetone * x) / 760 # fraction of acetone in gas
-    ans = [ele, p_acetone.round, p_ethanol.round, x.round(3), y.round(3)]
+    ans = [temp, p_acetone.round, p_ethanol.round, x.round(3), y.round(3)]
     print ans
     puts ' '
   end
 end
 
-temp = [56, 60, 64, 72, 76, 78.4]
-p give_back_x_y(temp)
+temps = [56, 60, 64, 72, 76, 78.4]
+p give_back_x_y(temps)
